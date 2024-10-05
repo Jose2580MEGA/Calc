61e124eaf142f9a9c1305c9b0ac0b5e9f958149f
Jose2580MEGA <josemeza23001@gmail.com>
2024-10-05 11:30:10 -0500

    calcver1

---
+++ script.js
@@ -0,0 +1,11 @@
+    var numero = 0
+    var cadnumero = ''
+    var operacion = 1
+
+     //1 es la suma
+     //2 es la resta
+
+    function captura(numero){
+    cadnumero += numero
+    document.getElementById("resultado").value =  cadnumero
+    }
